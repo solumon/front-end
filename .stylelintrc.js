51f2f1b18c6d16c99209128a757eb6a68b7aeec3
樊宗领 <fanzongling@up366.com>
2022-05-09 13:39:45 +0800

    eslint 配置

--- .stylelintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    extends: [
-        'stylelint-config-recess-order',
-        "stylelint-config-standard-scss",
-        "stylelint-config-standard-vue/scss"
-    ],
-};
---
+++ packages/eslint-config/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+    browser: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/recommended',
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    extraFileExtensions: ['.vue'],
+  },
+
+  plugins: ['@typescript-eslint', 'vue'],
+
+}
