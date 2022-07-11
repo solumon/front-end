1ae08c0d18c21731cc77e3dad147d065650eb58c
solumon <solumon@sina.com>
2022-07-12 01:12:37 +0800

    添加全局eslint

---
+++ packages/eslint-config-base/test/index.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="hello" style="display: flex;"></div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
--- packages/vue3-template/src/index/main.js
+++ packages/vue3-template/src/index/main.js
@@ -1,3 +1,9 @@
 const el = document.getElementById('app')
 
 el.innerText = '这是一个目录页哦'
+
+const x = () => {
+    console.log('ccc')
+}
+
+console.log(x)
