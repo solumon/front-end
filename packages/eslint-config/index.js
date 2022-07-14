33cc517337c96072576f6c833704fbe93e76b86d
樊宗领 <fanzongling@up366.com>
2022-07-14 13:53:36 +0800

    更新eslint配置

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,22 +1,27 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true,
-    jest: true,
-    browser: true,
-  },
-  extends: [
-    'airbnb-base',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:vue/recommended',
-  ],
-  parser: "vue-eslint-parser",
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    ecmaVersion: 2022,
-    sourceType: 'module',
-    extraFileExtensions: ['.vue'],
-  },
+    env: {
+        es6: true,
+        node: true,
+        jest: true,
+        browser: true
+    },
+    extends: [
+        'airbnb-base',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/recommended'
+    ],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        extraFileExtensions: ['.vue']
+    },
 
-  plugins: ['@typescript-eslint', 'vue'],
+    plugins: ['@typescript-eslint', 'vue'],
+    rules: {
+        indent: ['error', 4],
+        semi: ['error', 'never'],
+        'comma-dangle': ['error', 'never']
+    }
 }
